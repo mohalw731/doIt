e4f8f2ebf2662df9f071fb5b43eaecada4e2fddf
mohalw731 <mohalw731@gmail.com>
2024-07-23 23:04:43 +0200

    styling in hero and about

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,18 +2,18 @@ export default function Hero() {
   return (
     <main className="md:py-32 py-20">
       <section className="flex flex-col md:gap-6 gap-4">
-        <h1 className="md:text-8xl font-bold text-4xl">
+        <h1 className="md:text-8xl font-bold text-5xl">
           <span id="hero-text">A more humane</span> 
           <br />
           to-do list
         </h1>
-        <p className="text-slate-600 md:text-2xl max-w-[500px] ">
+        <p className="text-slate-600 md:text-2xl max-w-[500px] text-s">
           doIt is a back to basic to-do list focused on fast and delightful user
           experience.
         </p>
 
         <div className="">
-          <button className="bg-[#008ffd] md:py-3 text-white md:px-6 rounded-full px-4 py-2  hover:opacity-80 ">
+          <button className="bg-[#008ffd] md:py-3 text-white md:px-6 rounded-full px-4 py-2  hover:opacity-80 md:text-lg text-sm">
             Get Started
           </button>
         </div>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,14 +7,14 @@ export default function About() {
     <main>
       <Navbar />
 
-      <section className="md:py-32 py-20">
-        <div className="flex flex-col md:gap-6 gap-4">
-          <h1 className="md:text-8xl font-bold text-4xl">
+      <section className="md:py-32 py-28">
+        <div className="flex flex-col gap-6 ">
+          <h1 className="md:text-8xl font-bold text-5xl">
             <span id="hero-text">We’re building </span>
             <br />
             to-do list you’ll <br />enjoy using
           </h1>
-          <p className="text-slate-600 md:text-2xl max-w-[600px] font-semibold  ">
+          <p className="text-slate-600 md:text-2xl max-w-[600px] text-s, font-normal  ">
             Our mission with doIt is to solve current problems with to-do list
             apps by a more humane design approach and a completely natural user
             experience. <span className="text-[#b0b5ba]">
